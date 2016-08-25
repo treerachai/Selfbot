2e5086257f1af0103c7910ce70e6ec67355b3f47
Hyper-Coder <bryan.pikaardjr@outlook.com>
2016-08-25 12:31:44 -0400

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -1,21 +1,21 @@
-var myToken = ""
+var myToken = 'Y  O  U  R  -  T  O  K  E  N  -  H  E  R  E'
 
-var Discord = require('discord.js)
+var Discord = require('discord.js')
 var self = new Discord.Client()
 self.login(myToken)
-self.on("ready", () => {console.log("Connected!")})
-self.on("reconnecting", () => {console.log("Reconnecting!")})
+self.on('ready', () => {console.log('Connected!')})
+self.on('reconnecting', () => {console.log('Reconnecting!')})
 
 var list = {
-    "ping": message => {
-        message.channel.sendMessage("Pinging...")
+    'ping': message => {
+        message.channel.sendMessage('Pinging...')
             .then(msg => {
                 message.delete()
                 msg.edit(`Pong! \`Took ${msg.timestamp - message.timestamp}ms\``)
             })
     },
-    "eval": message => {
-        var code = message.content.slice("self:eval ".length)
+    'eval': message => {
+        var code = message.content.slice('self:eval '.length)
         try {
             message.edit(`\`INPUT:\`\n\`\`\`${code}\`\`\`\n\`OUTPUT:\`\n\`\`\`${eval(code)}\`\`\``)
         } catch(err) {
@@ -24,10 +24,10 @@ var list = {
     }
 }
 
-self.on("message", message => {
+self.on('message', message => {
     if (message.author.id !== self.user.id) return; // Make sure only you can use the bot
-    if (!message.content.startsWith("self:")) return; // Make sure it is a command
-    var command = list[message.content.split(" ")[0].substring(5)]; // Get the command
+    if (!message.content.startsWith('self:')) return; // Make sure it is a command
+    var command = list[message.content.split(' ')[0].substring(5)]; // Get the command
     if (!command) return; // Check to see if it is a command
     command(message); // Run the command
 });
