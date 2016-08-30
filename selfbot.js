af4e8de1163bcc1b1c27f335c15d91db3a2a48de
Hyper-Coder <bryan.pikaardjr@outlook.com>
2016-08-30 14:08:05 -0400

    Recreate Self Bot

--- selfbot.js
+++ selfbot.js
@@ -1,34 +1,55 @@
-var myToken = 'Y  O  U  R  -  T  O  K  E  N  -  H  E  R  E'
-var myPreifx = 'self:'
+// Inital Requiring
+const Discord = require('discord.js');
+const self = new Discord.Client();
 
-var Discord = require('discord.js')
-var self = new Discord.Client()
-self.login(myToken)
-self.on('ready', () => {console.log('Connected!')})
-self.on('reconnecting', () => {console.log('Reconnecting!')})
+// Variables
+const myToken = '  Y  o  u  r     T  o  k  e  n     H  e  r  e  ';
+const prefix = 'self:';
 
-var list = {
+// Create commands
+const commands = {
     'ping': message => {
-        message.channel.sendMessage('Pinging...')
+        message.channel.sendMessage('`SELF:` Pinging...')
             .then(msg => {
-                message.delete()
-                msg.edit(`Pong! \`Took ${msg.timestamp - message.timestamp}ms\``)
-            })
+                message.delete();
+                msg.edit(`\`SELF:\` Pong! \`Took ${msg.timestamp - message.timestamp}ms\``);
+            });
     },
-    'eval': message => {
-        var code = message.content.slice(myPrefix.length + 1)
+    'restart': message => {
+        // You must be using pm2 for this to work correctly
+        message.edit('`SELF:` Restarting!')
+            .then(() => process.exit());
+    },
+    'eval': (message, newContent) => {
+        const code = newContent.slice(newContent.search(' ') + 1);
+        if (!code.length) return message.reply('there\'s no code!');
         try {
-            message.edit(`\`INPUT:\`\n\`\`\`${code}\`\`\`\n\`OUTPUT:\`\n\`\`\`${eval(code)}\`\`\``)
+            message.edit(`\`INPUT:\`\n\`\`\`${code}\`\`\`\n\`OUTPUT:\`\n\`\`\`${eval(code)}\`\`\``);
         } catch(err) {
-            message.edit(`\`INPUT:\`\n\`\`\`${code}\`\`\`\n\`ERROR:\`\n\`\`\`${err}\`\`\``)
+            message.edit(`\`INPUT:\`\n\`\`\`${code}\`\`\`\n\`ERROR:\`\n\`\`\`${err}\`\`\``);
         }
     }
-}
+};
+
+// Receivers
+self.on('ready', () => {
+    console.log('Hey, I\'m ready to take commands!');
+});
+
+self.on('reconnecting', () => {
+    console.log('Oh no! I disconnected from Discord. I\'m attempting to reconnect.');
+});
 
 self.on('message', message => {
-    if (message.author.id !== self.user.id) return; // Make sure only you can use the bot
-    if (!message.content.startsWith(myPrefix)) return; // Make sure it is a command
-    var command = list[message.content.split(' ')[0].substring(myPrefix.length)]; // Get the command
-    if (!command) return; // Check to see if it is a command
-    command(message); // Run the command
+    if (!message.author.equals(self.user)) return; // Just to be sure the author is the self bot
+    if (!message.content.startsWith(prefix)) return; // Just to make sure a command is being used
+
+    const content = message.content.slice(prefix.length);
+
+    const command = commands[content.split(' ')[0]];
+    if (!command) return message.edit('`ERROR:` Invalid Command');
+    command(message, content);
 });
+
+// Initiate logging in to the bot
+self.login(myToken);
